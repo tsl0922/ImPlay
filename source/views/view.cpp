cc8f90d14013f1a077b584843aa7f818999ca6d8
Shuanglei Tao <tsl0922@gmail.com>
2023-02-02 13:06:16 +0800

    print NFD error on failure

--- source/views/view.cpp
+++ source/views/view.cpp
@@ -1,41 +1,78 @@
 // Copyright (c) 2022 tsl0922. All rights reserved.
 // SPDX-License-Identifier: GPL-2.0-only
 
+#include "helpers.h"
 #include "views/view.h"
 
 namespace ImPlay::Views {
 void View::openFile(std::vector<nfdu8filteritem_t> filters, std::function<void(nfdu8char_t*)> callback) {
-  NFD::Init();
+  if (NFD::Init() != NFD_OKAY) {
+    print("NFD Init Error: {}\n", NFD::GetError());
+    return;
+  }
   nfdchar_t* outPath;
-  if (NFD::OpenDialog(outPath, filters.data(), filters.size()) == NFD_OKAY) {
-    callback(outPath);
-    NFD::FreePath(outPath);
+  auto result = NFD::OpenDialog(outPath, filters.data(), filters.size());
+  switch (result) {
+    case NFD_OKAY:
+      callback(outPath);
+      NFD::FreePath(outPath);
+      break;
+    case NFD_ERROR:
+      print("NFD Error: {}\n", NFD::GetError());
+      break;
+    case NFD_CANCEL:
+    default:
+      break;
   }
   NFD::Quit();
 }
 
 void View::openFiles(std::vector<nfdu8filteritem_t> filters, std::function<void(nfdu8char_t*, int)> callback) {
-  NFD::Init();
+  if (NFD::Init() != NFD_OKAY) {
+    print("NFD Init Error: {}\n", NFD::GetError());
+    return;
+  }
   const nfdpathset_t* outPaths;
-  if (NFD::OpenDialogMultiple(outPaths, filters.data(), filters.size()) == NFD_OKAY) {
-    nfdpathsetsize_t numPaths;
-    NFD::PathSet::Count(outPaths, numPaths);
-    for (auto i = 0; i < numPaths; i++) {
-      nfdchar_t* path;
-      NFD::PathSet::GetPath(outPaths, i, path);
-      callback(path, i);
-    }
-    NFD::PathSet::Free(outPaths);
+  nfdpathsetsize_t numPaths;
+  auto result = NFD::OpenDialogMultiple(outPaths, filters.data(), filters.size());
+  switch (result) {
+    case NFD_OKAY:
+      NFD::PathSet::Count(outPaths, numPaths);
+      for (auto i = 0; i < numPaths; i++) {
+        nfdchar_t* path;
+        NFD::PathSet::GetPath(outPaths, i, path);
+        callback(path, i);
+      }
+      NFD::PathSet::Free(outPaths);
+      break;
+    case NFD_ERROR:
+      print("NFD Error: {}\n", NFD::GetError());
+      break;
+    case NFD_CANCEL:
+    default:
+      break;
   }
   NFD::Quit();
 }
 
 void View::openFolder(std::function<void(nfdu8char_t*)> callback) {
-  NFD::Init();
+  if (NFD::Init() != NFD_OKAY) {
+    print("NFD Init Error: {}\n", NFD::GetError());
+    return;
+  }
   nfdchar_t* outPath;
-  if (NFD::PickFolder(outPath) == NFD_OKAY) {
-    callback(outPath);
-    NFD::FreePath(outPath);
+  auto result = NFD::PickFolder(outPath);
+  switch (result) {
+    case NFD_OKAY:
+      callback(outPath);
+      NFD::FreePath(outPath);
+      break;
+    case NFD_ERROR:
+      print("NFD Error: {}\n", NFD::GetError());
+      break;
+    case NFD_CANCEL:
+    default:
+      return;
   }
   NFD::Quit();
 }
