95eae2c072bd40d6feac3b9d9a19a9496cc694b7
Shuanglei Tao <tsl0922@gmail.com>
2023-03-23 00:41:36 +0800

    make custom font work for Latin characters

--- source/views/debug.cpp
+++ source/views/debug.cpp
@@ -439,6 +439,7 @@ void Debug::Console::draw() {
     // - Split them into same height items would be simpler and facilitate random-seeking into your list.
     // - Consider using manual call to IsRectVisible() and skipping extraneous decoration from your items.
     ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1));  // Tighten spacing
+    ImGui::PushFont(ImGui::GetIO().Fonts->Fonts.back());
     if (copy_to_clipboard) ImGui::LogToClipboard();
     for (int i = 0; i < Items.Size; i++) {
       auto item = Items[i];
@@ -449,6 +450,7 @@ void Debug::Console::draw() {
       ImGui::PopStyleColor();
     }
     if (copy_to_clipboard) ImGui::LogFinish();
+    ImGui::PopFont();
 
     // Keep up at the bottom of the scroll region if we were already at the bottom at the beginning of the frame.
     // Using a scrollbar or mouse-wheel will take away from the bottom edge.
