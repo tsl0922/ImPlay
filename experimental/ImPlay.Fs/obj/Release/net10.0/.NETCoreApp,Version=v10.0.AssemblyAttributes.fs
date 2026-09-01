8253d2c964b0805322e9199421dd4c414ad83671
skonester <theskonester@aol.com>
2026-09-01 00:33:15 -0500

    update

--- experimental/ImPlay.Fs/obj/Release/net10.0/.NETCoreApp,Version=v10.0.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETCoreApp,Version=v10.0", FrameworkDisplayName=".NET 10.0")>]
-                do ()
